8d4a8615c0ae885226efe092765aa7493bc6787b
HarryCode <harisjavedawan123@gmail.com>
2024-04-24 21:04:00 +0500

    cors pos

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ dotenv.config();
 //database config
 connectDB();
 
-app.use(cors());
+
 //esmodule fix
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -26,7 +26,7 @@ const app = express();
 
 //middlewares
 
-
+app.use(cors());
 app.use(express.json());
 app.use(morgan("dev"));
 app.use(express.static(path.join(__dirname, "./client/build")));
