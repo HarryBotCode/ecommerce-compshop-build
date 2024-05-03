2fd319bfe0d1b7ed05fb0fae8e71e23b7208fe1c
HarryCode <harisjavedawan123@gmail.com>
2024-05-03 14:59:30 +0500

    updated server file

--- backend/server.js
+++ backend/server.js
@@ -8,11 +8,11 @@ import categoryRoute from "./routes/categoryRoute.js";
 import productRoute from "./routes/productRoute.js";
 import cors from "cors";
 import path from 'path';
-import { fileURLToPath } from 'url';
-import { dirname } from 'path';
+// import { fileURLToPath } from 'url';
+// import { dirname } from 'path';
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+// const __filename = fileURLToPath(import.meta.url);
+// const __dirname = dirname(__filename);
 //configure env
 dotenv.config();
 
@@ -26,7 +26,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(morgan("dev"));
-app.use(express.static(path.join(__dirname, './client/build')));
+// app.use(express.static(path.join(__dirname, './client/build')));
 
 //routes
 app.use("/api/v1/auth", authRoutes);
@@ -37,9 +37,9 @@ app.use("/api/v1/product", productRoute);
 app.get("/", (req, res) => {
   res.send("<h1>Welcome to Ecommerce App</h1>");
 });
-app.use('*',function(req, res){
-  res.sendFile(path.join(__dirname,'./client/build/index.html'))
-})
+// app.use('*',function(req, res){
+//   res.sendFile(path.join(__dirname,'./client/build/index.html'))
+// })
 // port
 const PORT = 8080;
 
